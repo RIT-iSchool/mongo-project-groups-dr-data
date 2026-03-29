02085c3182c88dec3d6540b8a5134203ab40dad0
safwan <ss7332@g.rit.edu>
2026-03-29 15:57:52 -0400

    fix route for server issue

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,1 +1,6 @@
 // juan task 2 and chris task 3
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
--- backend/routes/studies.js
+++ backend/routes/studies.js
@@ -1,1 +1,6 @@
 //maybe task 2 and 3 might need it , remove if not needed
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
